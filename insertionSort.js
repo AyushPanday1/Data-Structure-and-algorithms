a696b426d1a0c2f0e3572bf2c254ac28270e8fc3
AyushPanday1 <ayush7426883261@gmail.com>
2023-03-18 18:45:16 +0530

    Insertion Sort

---
+++ insertionSort.js
@@ -0,0 +1,38 @@
+//It is used when the quantity of elements is comparatively low.
+//Time complexity-0(n^2)
+//Space complexity-0(n)
+
+
+INPUT:
+
+var array = [12, 43, 87, 56, 89, 14];
+
+SOLUTION:
+
+function insertionSort(arr) {
+
+    //Start with 1 as we have to compare 0th index also.
+    for (let i = 1; i < arr.length; i++) {
+        let currentElement = arr[i];
+
+        let j = i - 1;
+
+        //Making the forwarded index to previous position.
+        while (j > -1 && currentElement < arr[j]) {
+            arr[j + 1] = arr[j];
+            j--
+        }
+
+        arr[j + 1] = currentElement
+    }
+
+    return arr
+
+}
+
+
+OUTPUT:
+insertionSort(array);
+console.log(array);
+
+//[ 12, 14, 43, 56, 87, 89 ]
