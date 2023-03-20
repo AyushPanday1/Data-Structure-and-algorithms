11680e9032cb4e87b22cbe7c43bab1b402e668fc
AyushPanday1 <ayush7426883261@gmail.com>
2023-03-20 11:26:18 +0530

    Time and space complexity

---
+++ aa.Time and Space complexity.js
@@ -0,0 +1,50 @@
+/*
+DSA is always practiced for making the program Time and space efficient.
+
+
+Time complexity - It is the time bound that a program takes to execute and return its result.
+
+for e.g.
+
+Calculate the sum upto 10;
+
+Normal approach:
+
+let sum = 0;
+for(let i=0;i<=10;i++){
+    sum+=i;
+}
+console.log(sum)
+
+T(C) = 0(n)
+
+
+
+Efficient approach:
+
+Sum = (N*(N+1))/2;
+
+T(C) = 0(1)
+
+
+It is calculated in Big-oh, Omega and theta notation.
+
+Big-oh notation 0(n) - Worst case or uppermost bound to solve a program.
+
+Big-Omega notation (Ω) - Best or efficient case or lowest bound.
+
+Big-Theta (Θ) - Average case or normal bound.
+
+
+====================================================================
+
+
+Space complexity:
+
+It is defined as space used by an algorithm to execute code. 
+Any extra space covered by a program makes the code slow.
+
+e.g. = 0(1)
+
+
+*/
